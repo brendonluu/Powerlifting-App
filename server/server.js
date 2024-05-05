553283784b350e143ae5faa4ef72d7c4e844539d
Abdul Hadi <2003abdulhadi@gmail.com>
2024-05-04 21:34:08 -0400

    changed server code

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
-import records from "./routes/record.js";
+import records from "./routes/dashboard.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/record", records);
+app.use("/dashboard", dashboard);
 
 // start the Express server
 app.listen(PORT, () => {
