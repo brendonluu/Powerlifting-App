2313236d08a04c447c233f1637532bdf203ca7bf
Abdul Hadi <2003abdulhadi@gmail.com>
2024-06-13 22:16:27 -0400

    added scaffolding for auth

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,26 @@
 import { NavLink } from "react-router-dom"
 import logo from "./../assets/tempLogo.png"
-import pfp from "./../assets/Default_pfp.svg"
 import './NavBar.css'
 import User from "./navbar_components/User"
+import { useState } from "react"
 
 function NavBar() {
+   const [loggedIn, setLoggedIn] = useState(false)
+
+   const logIn = () => {
+      setLoggedIn(current => true)
+   }
    return (
       <header className="header">
          <nav className="navContainer">
             <NavLink to='/' className="Logo">
                <img className="Logo" src={logo} alt="Name" />
             </NavLink>
-            <User />
+            {
+               loggedIn
+                  ? <User />
+                  : <button className="logIn" onClick={logIn}>Log In</button>
+            }
          </nav>
       </header>
 
